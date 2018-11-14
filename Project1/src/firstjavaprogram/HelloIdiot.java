132f652de8eccdb5ffc4cf2293bb13845a9e2d0c
LIKITHA <LIKITHA@LIKITHA-PC>
2018-11-14 17:00:30 +0530

    First commit

---
+++ Project1/src/firstjavaprogram/HelloIdiot.java
@@ -0,0 +1,10 @@
+package firstjavaprogram;
+
+public class HelloIdiot {
+   public static void main(String[] args) {
+	   System.out.println("Hello Daffa");
+	   System.out.println("hi ra ");
+}
+
+
+}
---
+++ Project1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author LIKITHA
+ *
+ */
+module Project1 {
+}
